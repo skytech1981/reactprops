93345486f855b4862f3eb5e8577bf5fd35f60865
sali itzhaki <sali1943@hotmail.com>
2020-07-29 13:12:35 +0300

    hello world

---
+++ app.js
@@ -0,0 +1,18 @@
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+        <p>hello world</p>
+         </div>
+      );
+    }
+  }
+
+
+
+
+
+
+
+  ReactDOM.render(<App />, document.getElementById('root'));
